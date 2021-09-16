d07cd2214d53a26f1a8d60d703e2a0518a48c2ac
Can202 <mgoopazo@gmail.com>
2021-09-16 16:17:11 -0300

    create-idur

---
+++ create-idur.py
@@ -0,0 +1,33 @@
+
+Name="create-idur"
+Version="v0.1"
+
+
+Maintainer="Can202"
+Contact="mgoopazo@gmail.com"
+
+Arch="all"
+
+Depends= ["python3", "coreutils", "curl"]
+Conflict= ["create-idur"]
+Description="""
+
+An Easy way to create idurs
+
+"""
+
+Install="""
+
+cd /tmp/
+rm -vrf create-idur.py
+curl -LO https://raw.githubusercontent.com/idur-package/create-idur/v0.1/create-idur.py
+chmod a+x create-idur.py
+cp create-idur.py /usr/bin/create-idur
+
+"""
+
+Remove="""
+
+rm -vrf /usr/bin/create-idur
+
+"""
