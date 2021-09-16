bf2af6445e67174c6c4005d60844d1be488bce74
Can202 <mgoopazo@gmail.com>
2021-09-16 09:36:07 -0300

    test

---
+++ test.py
@@ -0,0 +1,19 @@
+Name="test"
+Version="v0.1-beta.1"
+idurDepends=["wer"]
+Maintainer="Can202"
+
+Arch="all" #all, x86_64, i386, both
+
+Description="""
+test
+"""
+
+
+Install="""
+echo installed
+"""
+
+Remove="""
+echo removed
+"""
