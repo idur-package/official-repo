fbb7900fc49c1cd3b0fe0bf1b6d50aedcde35294
Can202 <mgoopazo@gmail.com>
2021-09-15 23:03:40 -0300

    opentictactoex

--- opentictactoex.py
+++ opentictactoex.py
@@ -17,7 +17,7 @@ cd /tmp/
 rm -vrf otttxtemp/
 mkdir -p otttxtemp/
 cd otttxtemp/
-curl -LO https://sourceforge.net/projects/opentictactoex/files/v0.5/opentictactoex_0.5-amd64.deb/download
+curl -LO https://sourceforge.net/projects/opentictactoex/files/v0.5/OpenTicTacToeX.x86/download
 curl -LO https://raw.githubusercontent.com/OpenTicTacToeX/package/main/deb64/usr/share/icons/hicolor/256x256/apps/opentictactoex.png
 curl -LO https://raw.githubusercontent.com/OpenTicTacToeX/package/main/deb64/usr/share/applications/opentictactoex.desktop
 chmod a+x opentictactoex.desktop
@@ -29,11 +29,10 @@ rm -vrf /tmp/otttxtemp/
 """
 
 Install32="""
-cd /tmp/
-rm -vrf otttxtemp/
-mkdir -p otttxtemp/
-cd otttxtemp/
-curl -LO https://sourceforge.net/projects/opentictactoex/files/v0.5/opentictactoex_0.5-i386.deb/download
+rm -vrf /tmp/otttxtemp/
+mkdir -p /tmp/otttxtemp/
+cd /tmp/otttxtemp/
+curl -LO https://sourceforge.net/projects/opentictactoex/files/v0.5/OpenTicTacToeX.x86_64/download
 curl -LO https://raw.githubusercontent.com/OpenTicTacToeX/package/main/deb64/usr/share/icons/hicolor/256x256/apps/opentictactoex.png
 curl -LO https://raw.githubusercontent.com/OpenTicTacToeX/package/main/deb64/usr/share/applications/opentictactoex.desktop
 chmod a+x opentictactoex.desktop
