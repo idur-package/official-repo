078989e05f8bb4617acf7863d5f95e00767fc078
Can202 <mgoopazo@gmail.com>
2021-09-16 16:47:38 -0300

    laer

---
+++ laer.py
@@ -0,0 +1,39 @@
+
+Name="laer"
+Version="v0.2"
+
+
+Maintainer="Can202"
+Contact="mgoopazo@gmail.com"
+
+Arch="all"
+
+Depends= ["vim", "bash", "neofetch"]
+Conflict= ["laer"]
+Description="""
+
+clear command, but personalized
+
+"""
+
+Install="""
+
+mkdir -p /etc/laer
+rm -vrf /tmp/laer
+mkdir -p /tmp/laer
+cd /tmp/laer
+curl -OL https://raw.githubusercontent.com/Can202/laer/v0.2/laer
+curl -OL https://raw.githubusercontent.com/Can202/laer/v0.2/bashinstaller/laer-delete
+
+chmod a+x laer-delete
+chmod a+x laer
+cp laer /usr/bin/laer
+cp laer-delete /usr/bin/laer-delete
+
+"""
+
+Remove="""
+
+laer-delete
+
+"""
