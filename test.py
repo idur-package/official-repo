6a8ac7d5070063e11348a02d89bd26e1c1ffa6e7
Can202 <mgoopazo@gmail.com>
2021-09-18 01:03:11 -0300

    test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 Name="test"
 Version="0.5"
 Depends=["python3", "bash", "curl", "rec/xterm lxterminal"]
-idurDepends=["idur-dev idur-stable"]
+idurDepends=["idur-stable idur-dev"]
 Conflict=["wer", "test"]
 
 Maintainer="Can202"
