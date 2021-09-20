11f7616e26b4885d4619d361379fca64328c0b01
Can202 <mgoopazo@gmail.com>
2021-09-20 13:10:28 -0300

    dunpress

---
+++ dunpress.py
@@ -0,0 +1,30 @@
+Name="dunpress"
+Version="v0.1"
+
+Maintainer="Can202"
+Contact="mgoopazo@hotmail.com"
+
+Arch="all"
+
+
+License="https://raw.githubusercontent.com/Can202/dunpress/main/LICENSE"
+Depends=["unzip", "tar", "python3"]
+idurDepends=["idur-pkg"]
+Conflict=["dunpress"]
+Description="""
+uncompress tarballs and zip with one command.
+
+"""
+
+Install="""
+
+idur-pkg read https://raw.githubusercontent.com/Can202/dunpress/v0.1/src/dumpress.py > /usr/bin/dunpress
+chmod a+x /usr/bin/dunpress
+
+"""
+
+Remove="""
+
+idur-pkg rm /usr/bin/dunpress
+
+"""
