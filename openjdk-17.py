780e829f3372b1d987105f04b441db5d978095f8
Can202 <mgoopazo@gmail.com>
2021-09-20 17:03:19 -0300

    openjdk-17

--- openjdk-17.py
+++ openjdk-17.py
@@ -1,6 +1,6 @@
 
 Name="openjdk-17"
-Version="35"
+Version="35.1"
 
 
 Maintainer="Can202"
@@ -24,7 +24,7 @@ cd /tmp/idur-jdk17-tmp/
 idur-pkg download https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
 tar -xvzf openjdk-17_linux-x64_bin.tar.gz
 cp -r /tmp/idur-jdk17-tmp/jdk-17/ /opt/openjdk-17
-idur-pkg read https://raw.githubusercontent.com/idur-package/media/b0d031a2c2bd52406446d8521704faa67075e811/openjdk-17/openjdk-17 > /usr/bin/openjdk-17
+idur-pkg read https://raw.githubusercontent.com/idur-package/media/bc4b85aa849ce60bc8ad8b93ea64b89cb97b01bb/openjdk-17/openjdk-17 > /usr/bin/openjdk-17
 chmod a+x /usr/bin/openjdk-17
 cd ..
 idur-pkg rm-tmp jdk17
