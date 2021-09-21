10f7868dff1e884beb3e1f177cbbc130190a7f4a
Can202 <mgoopazo@gmail.com>
2021-09-21 16:18:36 -0300

    idur-dev

--- idur-stable.py
+++ idur-stable.py
@@ -1,5 +1,5 @@
 Name="idur-stable"
-Version="v0.1.8"
+Version="v0.1.8-dev.1"
 Depends=["curl", "bash", "python3", "coreutils", "git"]
 
 
@@ -22,7 +22,7 @@ cd /tmp/
 rm -vrf idurtemp/
 mkdir -p idurtemp/
 cd idurtemp/
-curl -LO https://raw.githubusercontent.com/idur-package/idur/v0.1.8/src/idur.py
+curl -LO https://raw.githubusercontent.com/idur-package/idur/4a0562df9d9ef7fda5450f05adea9ed41bd96b7f/src/idur.py
 chmod a+x idur.py
 cp idur.py /usr/bin/idur
 rm -vrf idurtemp/
