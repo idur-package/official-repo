419b1967d96bdab84169d21f312628a947c956d6
Can202 <mgoopazo@gmail.com>
2021-09-22 20:27:06 -0300

    create-idur v0.7

--- create-idur.py
+++ create-idur.py
@@ -1,6 +1,6 @@
 
 Name="create-idur"
-Version="v0.6"
+Version="v0.7"
 
 Time="short"
 Maintainer="Can202"
@@ -20,7 +20,7 @@ Install="""
 
 cd /tmp/
 rm -vrf create-idur.py
-curl -LO https://raw.githubusercontent.com/idur-package/create-idur/v0.6/create-idur.py
+curl -LO https://raw.githubusercontent.com/idur-package/create-idur/v0.7/create-idur.py
 chmod a+x create-idur.py
 cp create-idur.py /usr/bin/create-idur
 
