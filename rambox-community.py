a9184b8dc8c66b392c6020d194942ef8c388115f
Can202 <mgoopazo@gmail.com>
2021-09-23 19:21:09 -0300

    rambox

--- rambox-community.py
+++ rambox-community.py
@@ -17,46 +17,57 @@ Free and Open Source messaging and emailing app that combines common web applica
 """
 
 Install64="""
-mkdir -p /opt/rambox-community
-cd /opt/rambox-community
+mkdir -p /opt/idur/share/program/rambox-community
+cd /opt/idur/share/program/rambox-community
 idur-pkg download https://github.com/ramboxapp/community-edition/releases/download/0.7.9/Rambox-0.7.9-linux-x86_64.AppImage
-chmod a+x Rambox-0.7.9-linux-x86_64.AppImage
+idur-pkg exec Rambox-0.7.9-linux-x86_64.AppImage
 echo "#!/usr/bin/bash
-cd /opt/rambox-community/
+cd /opt/idur/share/program/rambox-community/
 ./Rambox-0.7.9-linux-x86_64.AppImage
-" > /opt/rambox-community/rambox
-chmod a+x /opt/rambox-community/rambox
-ln /opt/rambox-community/rambox /usr/bin/rambox
+" > /opt/idur/share/program/rambox-community/rambox
+idur-pkg exec /opt/idur/share/program/rambox-community/rambox
+ln /opt/idur/share/program/rambox-community/rambox /usr/bin/rambox
 
 idur-pkg download https://raw.githubusercontent.com/ramboxapp/community-edition/master/resources/logo/256x256.png
-mv 256x256.png /usr/share/icons/hicolor/256x256/apps/rambox.png
+mv 256x256.png /opt/idur/share/icons/rambox.png
 
 echo "[Desktop Entry]
 Name=Rambox
-Exec=rambox
-Icon=rambox
+Exec=idur-exec rambox
+Icon=/opt/idur/share/icons/rambox.png
 Categories=Network
 Type=Application" > /usr/share/applications/rambox.desktop
-chmod a+x /usr/share/applications/rambox.desktop
+idur-pkg exec /usr/share/applications/rambox.desktop
 """
 
 Install32="""
-mkdir -p /opt/rambox-community
-cd /opt/rambox-community
+mkdir -p /opt/idur/share/program/rambox-community
+cd /opt/idur/share/program/rambox-community
 idur-pkg download https://github.com/ramboxapp/community-edition/releases/download/0.7.9/Rambox-0.7.9-linux-i386.AppImage
-chmod a+x Rambox-0.7.9-linux-i386.AppImage
+idur-pkg exec Rambox-0.7.9-linux-i386.AppImage
 echo "#!/usr/bin/bash
-cd /opt/rambox-community/
+cd /opt/idur/share/program/rambox-community/
 ./Rambox-0.7.9-linux-i386.AppImage
-" > /opt/rambox-community/rambox
-chmod a+x /opt/rambox-community/rambox
-ln /opt/rambox-community/rambox /usr/bin/rambox
+" > /opt/idur/share/program/rambox-community/rambox
+idur-pkg exec /opt/idur/share/program/rambox-community/rambox
+ln /opt/idur/share/program/rambox-community/rambox /usr/bin/rambox
+
+idur-pkg download https://raw.githubusercontent.com/ramboxapp/community-edition/master/resources/logo/256x256.png
+mv 256x256.png /opt/idur/share/icons/rambox.png
+
+echo "[Desktop Entry]
+Name=Rambox
+Exec=idur-exec rambox
+Icon=/opt/idur/share/icons/rambox.png
+Categories=Network
+Type=Application" > /usr/share/applications/rambox.desktop
+idur-pkg exec /usr/share/applications/rambox.desktop
 """
 
 Remove="""
-idur-pkg rm /opt/rambox-community/
-idur-pkg rm /usr/bin/rambox
-idur-pkg rm /usr/share/icons/hicolor/256x256/apps/rambox.png
+idur-pkg rm /opt/idur/share/program/rambox-community/
+idur-pkg rm /opt/idur/bin/rambox
+idur-pkg rm /opt/idur/share/icons/rambox.png
 idur-pkg rm /usr/share/applications/rambox.desktop
 
 """
