c0e5730ccb87585a295155116b683321832a28bf
Can202 <mgoopazo@gmail.com>
2021-09-23 16:37:31 -0300

    pixelora idur developer items

--- pixelorama.py
+++ pixelorama.py
@@ -11,7 +11,7 @@ Time="short"
 
 License="https://raw.githubusercontent.com/Orama-Interactive/Pixelorama/master/LICENSE"
 Depends= ["libxcursor1", "tar", "coreutils"]
-idurDepends= ["idur-pkg"]
+idurDepends= ["idur-pkg", "idur-exec"]
 Conflict= ["pixelorama"]
 Description="""
 A free & open-source 2D sprite editor, made with the Godot Engine! 
@@ -20,33 +20,34 @@ A free & open-source 2D sprite editor, made with the Godot Engine!
 
 Install64="""
 idur-pkg tmp pixelorama
-cd /tmp/idur-pixelorama-tmp
+cd $(idur-pkg dp pixelorama)
 idur-pkg download https://github.com/Orama-Interactive/Pixelorama/releases/download/v0.9/Pixelorama.Linux-64bit.tar.gz
-tar -xvzf Pixelorama.Linux-64bit.tar.gz
+idur-pkg uncompress Pixelorama.Linux-64bit.tar.gz
 idur-pkg rm Pixelorama.Linux-64bit.tar.gz
 mv linux-64bit/ pixelorama
-cp -r pixelorama/ /usr/lib64/
+idur-pkg copy pixelorama/ /opt/idur/
 echo "#!/bin/bash
-cd /usr/lib64/pixelorama/
-./Pixelorama.x86_64" > /usr/bin/pixelorama
-chmod a+x /usr/bin/pixelorama
+cd /opt/idur/pixelorama/
+./Pixelorama.x86_64" > /opt/idur/bin/pixelorama
+idur-pkg exec /opt/idur/bin/pixelorama
 idur-pkg download https://raw.githubusercontent.com/Orama-Interactive/Pixelorama/master/assets/graphics/icons/icon.png
-cp icon.png /usr/share/icons/hicolor/256x256/apps/pixelorama.png
+idur-pkg copy icon.png /opt/idur/pixelorama/icon.png
 echo "[Desktop Entry]
 Name=Pixelorama
-Exec=pixelorama
-Icon=pixelorama
+Exec=idur-exec pixelorama
+Icon=/opt/idur/pixelorama/icon.png
 Type=Application
 Categories=Graphics;2DGraphics;" > /usr/share/applications/pixelorama.desktop
-chmod a+x /usr/share/applications/pixelorama.desktop
+idur-pkg exec /usr/share/applications/pixelorama.desktop
+idur-pkg rm-tmp pixelorama
 """
 
 Remove="""
 
-idur-pkg rm /usr/lib64/pixelorama/Pixelorama.x86_64
-idur-pkg rm /usr/lib64/pixelorama/Pixelorama.pck
+idur-pkg rm /opt/idur/pixelorama/Pixelorama.x86_64
+idur-pkg rm /opt/idur/pixelorama/Pixelorama.pck
 idur-pkg rm /usr/share/applications/pixelorama.desktop
-idur-pkg rm /usr/share/icons/hicolor/256x256/apps/pixelorama.png
-idur-pkg rm /usr/bin/pixelorama
+idur-pkg rm /opt/idur/pixelorama/icon.png
+idur-pkg rm /opt/idur/bin/pixelorama
 
 """
