630d96e8b6aab6591f37424228aaf9fab706bf67
Can202 <mgoopazo@gmail.com>
2021-09-23 22:41:26 -0300

    test

---
+++ test.py
@@ -0,0 +1,28 @@
+Name="test"
+Version="1"
+
+Maintainer="d"
+Contact="d"
+
+Arch="all"
+
+Time="short"
+
+Conflict=["test"]
+Description="""
+
+description here
+
+"""
+
+Install="""
+
+# Install instructions here (bash)
+
+"""
+
+Remove="""
+
+# Remove instructions here (bash)
+
+"""
